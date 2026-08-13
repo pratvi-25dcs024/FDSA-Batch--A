7d95bb3be4583cdc5285570da3bafe28b2e12e57
pratvi-25dcs024 <25dcs024@charusat.edu.in>
2026-08-13 15:44:47 +0530

    Add files via upload

---
+++ dsapractical1.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, h;
+
+    cout << "Enter number of items: ";
+    cin >> n;
+
+    vector<int> items(n);
+
+    cout << "Enter items: ";
+    for (int i = 0; i < n; i++) {
+        cin >> items[i];
+    }
+
+    cout << "Enter hours: ";
+    cin >> h;
+
+    h = h % n;
+
+    for (int i = 0; i < h; i++) {
+        int first = items[0];
+
+        for (int j = 0; j < n - 1; j++) {
+            items[j] = items[j + 1];
+        }
+
+        items[n - 1] = first;
+    }
+
+    cout << "Final display: ";
+    for (int i = 0; i < n; i++) {
+        cout << items[i] << " ";
+    }
+
+    return 0;
+}
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, h;
+
+    cout << "Enter number of items: ";
+    cin >> n;
+
+    vector<int> items(n);
+
+    cout << "Enter items: ";
+    for (int i = 0; i < n; i++) {
+        cin >> items[i];
+    }
+
+    cout << "Enter hours: ";
+    cin >> h;
+
+    h = h % n;
+
+    for (int i = 0; i < h; i++) {
+        int first = items[0];
+
+        for (int j = 0; j < n - 1; j++) {
+            items[j] = items[j + 1];
+        }
+
+        items[n - 1] = first;
+    }
+
+    cout << "Final display: ";
+    for (int i = 0; i < n; i++) {
+        cout << items[i] << " ";
+    }
+
+    return 0;
+}
